d075d790fd1ad358ead89372d4086fc20d68e856
claasklar <git@claasklar.de>
2020-11-30 22:36:41 +0100

    Add manage_devices_view
    This view displays a list of devices and a button to add a new device.

---
+++ js/views/manage_devices_view.js
@@ -0,0 +1,58 @@
+/* global Whisper, getAccountManager, i18n */
+
+/* eslint-disable more/no-then */
+
+// eslint-disable-next-line func-names
+(function() {
+  window.Whisper = window.Whisper || {};
+
+  Whisper.ManageDevicesView = Whisper.View.extend({
+    templateName: 'manageDevices',
+    className: 'full-screen-flow',
+    initialize() {
+      this._outer_render();
+    },
+    _outer_render() {
+      Whisper.View.prototype.render.call(this);
+      this.render();
+    },
+    render() {
+      const accountManager = getAccountManager();
+      if (!accountManager.isStandaloneDevice()) {
+        this.$('.button').hide();
+      }
+      accountManager.getDevices().then(devices => {
+        devices.forEach(device => {
+          device.name.then(name => {
+            const deviceModel = new Whisper.Device({
+              id: device.id,
+              name,
+              accountManager,
+            });
+            deviceModel.on('removeDevice', this.render);
+            const deviceListRow = new Whisper.DeviceListRowView({
+              model: deviceModel,
+            });
+            this.$('#linkedDevicesTable').append(deviceListRow.el);
+          });
+        });
+      });
+    },
+    render_attributes() {
+      return {
+        linkedDevices: i18n('linkedDevices'),
+        linkNewDevice: i18n('linkNewDevice'),
+      };
+    },
+    events: {
+      'click .close': 'onClose',
+      'click .button': 'onAddNewDevice',
+    },
+    onAddNewDevice() {
+      Whisper.events.trigger('addDevice');
+    },
+    onClose() {
+      this.$el.trigger('openInbox');
+    },
+  });
+})();
