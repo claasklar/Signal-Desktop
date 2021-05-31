ae38d5f1b2a8c6af9c711274ef2bad42c599f544
claasklar <github@claasklar.de>
2021-05-31 16:05:16 +0200

    Bug/#26 cant register standalone application (#27)
    Loads CAPTCHA to enable phone number verification
    
    Resolves #26
    Co-authored-by: Kevin J. Sung <kevjsung@umich.edu>

--- ts/textsecure/WebAPI.ts
+++ ts/textsecure/WebAPI.ts
@@ -864,8 +864,14 @@ export type WebAPIType = {
   registerSupportForUnauthenticatedDelivery: () => Promise<any>;
   removeDevice: (id: string) => Promise<void>;
   removeSignalingKey: () => Promise<void>;
-  requestVerificationSMS: (number: string) => Promise<any>;
-  requestVerificationVoice: (number: string) => Promise<any>;
+  requestVerificationSMS: (
+    number: string,
+    captchaToken?: string
+  ) => Promise<any>;
+  requestVerificationVoice: (
+    number: string,
+    captchaToken?: string
+  ) => Promise<any>;
   sendMessages: (
     destination: string,
     messageArray: Array<MessageType>,
@@ -1332,19 +1338,33 @@ export function initialize({
       });
     }
 
-    async function requestVerificationSMS(number: string) {
+    async function requestVerificationSMS(
+      number: string,
+      captchaToken?: string
+    ) {
+      let urlParameters = `/sms/code/${number}?client=desktop`;
+      if (captchaToken) {
+        urlParameters += `&captcha=${captchaToken}`;
+      }
       return _ajax({
         call: 'accounts',
         httpType: 'GET',
-        urlParameters: `/sms/code/${number}`,
+        urlParameters,
       });
     }
 
-    async function requestVerificationVoice(number: string) {
+    async function requestVerificationVoice(
+      number: string,
+      captchaToken?: string
+    ) {
+      let urlParameters = `/voice/code/${number}?client=desktop`;
+      if (captchaToken) {
+        urlParameters += `&captcha=${captchaToken}`;
+      }
       return _ajax({
         call: 'accounts',
         httpType: 'GET',
-        urlParameters: `/voice/code/${number}`,
+        urlParameters,
       });
     }
 
